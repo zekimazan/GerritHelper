963b6569ad3d757b053bd10cd2a57e54be8ba612
cgerede <Watney@SkyNet>
2017-08-04 18:41:57 +0300

    Add IndentationVarianceAnalyzer

--- src/main/java/csv/analyzer/IndendationVarianceAnalyzer.java
+++ src/main/java/csv/analyzer/IndendationVarianceAnalyzer.java
@@ -1,6 +1,12 @@
 package csv.analyzer;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import csv.analyzer.thirdparty.diff_match_patch.Diff;
+import csv.analyzer.thirdparty.diff_match_patch.Operation;
 
 /**
  * Note: Indentation unit needs to be detected first. Each indentation
@@ -25,32 +31,79 @@ import java.io.File;
  *
  */
 public class IndendationVarianceAnalyzer extends Analyzer {
-
+	Map<Integer, Integer> indentationBucketCount =
+			new TreeMap<Integer, Integer>();
+	Map<Integer, Integer> indentationVariance =
+			new TreeMap<Integer, Integer>();
+	
 	@Override
-	public void _beforeAnalyzeChange(int changeId) {
-		// TODO Auto-generated method stub
-
-	}
+	public void _beforeAnalyzeChange(int changeId) {}
 
 	@Override
 	public void _analyzeFile(int changeId, File origin, File proposed,
-			TextFileCache textFileContentCache) {
-		// TODO Auto-generated method stub
+			TextFileCache cache) {
+		LinkedList<Diff> diffs = cache.getDiff(origin, proposed);
+		int indentation = 0;
+		Map<Integer, Integer> indentationBuckets =
+				new TreeMap<Integer, Integer>();
 
+		boolean lineBeginning;
+		for (Diff diff : diffs) {
+			if (diff.operation == Operation.INSERT) {
+				lineBeginning = true;
+				indentation = 0;
+				for (int i = 0; i < diff.text.length(); i++) {
+					if (lineBeginning) {
+						switch(diff.text.charAt(i)) {
+							case '\n':
+								lineBeginning = true;
+								indentation = 0;
+								break;
+							case ' ':
+							case '\t':
+								if (lineBeginning) {
+									indentation++;
+								}
+								break;
+							default:
+								lineBeginning = false;
+								int count = 0;
+								if (indentationBuckets.containsKey(indentation)) {
+									count = indentationBuckets.get(indentation);
+								}
+								indentationBuckets.put(indentation, count + 1);
+								indentation = 0;
+								break;
+						}
+					} else if(diff.text.charAt(i) == '\n') {
+						lineBeginning = true;
+					}
+				}
+			}
+		}
+		indentationBucketCount.put(changeId, indentationBuckets.size());
 	}
 
 	@Override
-	public void _afterAnalyzeChange(int changeId) {
-		// TODO Auto-generated method stub
-
-	}
+	public void _afterAnalyzeChange(int changeId) {}
 
 	@Override
 	public AnalyzerDumper getDumper() {
-		// TODO Auto-generated method stub
-		return null;
+		return new AnalyzerDumper() {
+			
+			public String getColumns() {
+				return "IndentationBucketCount";
+			}
+			
+			public void appendDataForChange(int id, StringBuffer data) {
+				data.append(",").append(indentationBucketCount.get(id));
+			}
+		};
 	}
 
 	@Override
-	public void reset() {}
+	public void reset() {
+		indentationBucketCount.clear();
+		indentationVariance.clear();
+	}
 }
---
+++ src/main/java/csv/analyzer/IndendationVarianceAnalyzerTest.java
@@ -0,0 +1,99 @@
+package csv.analyzer;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+public class IndendationVarianceAnalyzerTest extends TestCase {
+	IndendationVarianceAnalyzer analyzer = new IndendationVarianceAnalyzer();
+	final int CHANGE_ID = 1, CHANGE_ID_2 = 2;
+
+	public void testGetColumns() {
+		analyzer.getDumper().getColumns().equals("IndentationBucketCount");
+	}
+
+	public void testSimple() {
+		TextFileCache cache = new TextFileCache();
+		File origin = new File("origin");
+		File proposed = new File("proposed");
+		cache.textFileContentCache.put(origin, "did not change.did not change.");
+		cache.textFileContentCache.put(proposed,
+				"did not change." +
+				"    4 spaces indented\n" +
+				"      6 spaces indented\n" +
+				"0 spaces indented\n" +
+				"\t\t 2 tabs and 1 space indented\n" +
+				"did not change.");
+
+		analyzer._beforeAnalyzeChange(CHANGE_ID);
+		analyzer._analyzeFile(CHANGE_ID, origin, proposed, cache);
+		analyzer._afterAnalyzeChange(CHANGE_ID);
+
+		StringBuffer data = new StringBuffer();
+		analyzer.getDumper().appendDataForChange(CHANGE_ID, data);
+		assertEquals(",4", data.toString());  // Buckets are: 4, 6, 0, 3
+	}
+
+	public void testNoIndentation() {
+		TextFileCache cache = new TextFileCache();
+		File origin = new File("origin");
+		File proposed = new File("proposed");
+		cache.textFileContentCache.put(origin, "did not change.did not change.");
+		cache.textFileContentCache.put(proposed,
+				"did not change." +
+				"no indentation\n" +
+				"no indentation\n" +
+				"did not change.");
+
+		analyzer._beforeAnalyzeChange(CHANGE_ID);
+		analyzer._analyzeFile(CHANGE_ID, origin, proposed, cache);
+		analyzer._afterAnalyzeChange(CHANGE_ID);
+
+		StringBuffer data = new StringBuffer();
+		analyzer.getDumper().appendDataForChange(CHANGE_ID, data);
+		assertEquals(",1", data.toString());  // Buckets are: 0
+	}
+
+	public void testSingleLine() {
+		TextFileCache cache = new TextFileCache();
+		File origin = new File("origin");
+		File proposed = new File("proposed");
+		cache.textFileContentCache.put(
+				origin, "did not change.did not change.");
+		cache.textFileContentCache.put(proposed,
+				"did not change." +
+				"no indentation" +
+				"did not change." +
+				"no indentation");
+
+		analyzer._beforeAnalyzeChange(CHANGE_ID);
+		analyzer._analyzeFile(CHANGE_ID, origin, proposed, cache);
+		analyzer._afterAnalyzeChange(CHANGE_ID);
+
+		StringBuffer data = new StringBuffer();
+		analyzer.getDumper().appendDataForChange(CHANGE_ID, data);
+		assertEquals(",1", data.toString());  // Buckets are: 0
+	}
+
+	public void testMultipleLinesAndDiffs() {
+		TextFileCache cache = new TextFileCache();
+		File origin = new File("origin");
+		File proposed = new File("proposed");
+		cache.textFileContentCache.put(
+				origin, "did not change.\ndid not change.");
+		cache.textFileContentCache.put(proposed,
+				"did not change.\n" +
+				" 1 space indentation   \n" +
+				"did not change.\n" +
+				"\n\n" +
+				"  2 spaces indentation");
+
+		analyzer._beforeAnalyzeChange(CHANGE_ID);
+		analyzer._analyzeFile(CHANGE_ID, origin, proposed, cache);
+		analyzer._afterAnalyzeChange(CHANGE_ID);
+
+		StringBuffer data = new StringBuffer();
+		analyzer.getDumper().appendDataForChange(CHANGE_ID, data);
+		assertEquals(",2", data.toString());  // Buckets are: 1, 2
+	}
+}
